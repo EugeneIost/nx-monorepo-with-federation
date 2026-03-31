ffdb63ae7e9040c4c93e215cbbc2950b8bf8c92f
Eugene <iostevg@safe-line.ru>
2026-03-31 19:47:19 +0300

    fix: fix title

--- apps/remote1/src/app/nx-welcome.tsx
+++ apps/remote1/src/app/nx-welcome.tsx
@@ -433,7 +433,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there VERSION 2, </span>
+              <span> Hello there VERSION 555, </span>
               Welcome {title} 👋
               <Checkbox />
             </h1>
